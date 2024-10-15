67dd87bc9b8381f8ce09d5367d26af8b0c58de10
Rovi <sergirovisu74@gmail.com>
2024-10-16 00:04:03 +0200

    added style about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,27 +1,46 @@
 import React from 'react'
 import '../styles/about.css'
 import rovi from '../assets/photo.jpg'
+import { FaLinkedin } from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa'
 
 const About = () => {
 	return (
 		<section id='about'>
 			<div className='container'>
-				<div className='about-div'>
-					<img id='rovi' src={rovi} alt='This is me!' />
+				<div className='about-content'>
+					<img
+						className='profile-pic'
+						src={rovi}
+						alt='Sergi Roviralta'
+					/>
 					<div className='presentation'>
 						<h4>Hi, I'm</h4>
 						<h1>Sergi Roviralta</h1>
 						<h3>Full Stack Developer</h3>
 						<div className='buttons'>
-							<button>Download CV</button>
-							<button>Contact Info</button>
+							<a href='/cv.pdf' className='button' download>
+								Download CV
+							</a>
+							<a href='#contact' className='button'>
+								Contact Info
+							</a>
 						</div>
-						<div
-							className='
-						about-icons'
-						>
-							<img src='' alt='Linkedin icon' />
-							<img src='' alt='Github icon' />
+						<div className='about-icons'>
+							<a
+								href='https://linkedin.com'
+								target='_blank'
+								rel='noreferrer'
+							>
+								<FaLinkedin />
+							</a>
+							<a
+								href='https://github.com'
+								target='_blank'
+								rel='noreferrer'
+							>
+								<FaGithub />
+							</a>
 						</div>
 					</div>
 				</div>
