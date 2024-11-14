7e9a33eb52fba37ce7a3f234cd44f11e0056d8e6
Rovi <sergirovisu74@gmail.com>
2024-11-14 23:53:45 +0100

    fix responsive and style design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header/Header";
 import About from "./components/About/About";
+import Project from "./components/Project/Project";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <About></About>
+      <Project></Project>
     </div>
   );
 }
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,14 +1,33 @@
 import React from "react";
 import "./About.css";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const About = () => {
   return (
     <section id="about">
-      <h1 className="presentation">
-        <span class="intro">Hi I am&nbsp;</span>
-        <span class="name">Sergi Roviralta.</span>
-      </h1>
+      <div className="presentation">
+        <h1>
+          <span className="intro">Hi I am&nbsp;</span>
+          <span className="name">Sergi Roviralta,</span>
+        </h1>
+      </div>
       <h2 className="role">A Full Stack Engineer</h2>
+      <p className="resume">
+        Computer science engineer skilled in full stack development and hybrid
+        applications with C, Java, Python, and JavaScript. Passionate about
+        high-quality solutions and eager to tackle new challenges.
+      </p>
+      <div className="buttons">
+        <button id="cv" title="Download Resume">
+          Resume
+        </button>
+        <span className="icon" aria-label="Github" title="Github">
+          <FaGithub />
+        </span>
+        <span className="icon" aria-label="LinkedIn" title="LinkedIn">
+          <FaLinkedin />
+        </span>
+      </div>
     </section>
   );
 };
