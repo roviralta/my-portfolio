b526b98399c55a3910c788d4b4d21ac449d24343
Sergi Roviralta <sergirovisu74@gmail.com>
2025-02-05 00:23:55 +0100

    added hamburguer menu functional

--- src/components/Header/HamHeader.js
+++ src/components/Header/HamHeader.js
@@ -1,10 +1,43 @@
-import React from 'react'
-import { IoMenu } from 'react-icons/io5'
+import React, { useState } from 'react'
+import { IoMenu, IoClose } from 'react-icons/io5'
+import './HamHeader.css'
 
 const HamHeader = () => {
+	const [isOpen, setIsOpen] = useState(false)
+
+	const toggleMenu = () => {
+		setIsOpen(!isOpen)
+	}
+
 	return (
-		<div>
-			<IoMenu></IoMenu>
+		<div className='ham-container'>
+			<div className='ham-button' onClick={toggleMenu}>
+				{isOpen ? <IoClose size={30} /> : <IoMenu size={30} />}
+			</div>
+			<nav className={`ham-menu-list ${isOpen ? 'open' : ''}`}>
+				<ul>
+					<li>
+						<a href='#about-me' onClick={toggleMenu}>
+							About me
+						</a>
+					</li>
+					<li>
+						<a href='#experience' onClick={toggleMenu}>
+							Experience
+						</a>
+					</li>
+					<li>
+						<a href='#projects' onClick={toggleMenu}>
+							Projects
+						</a>
+					</li>
+					<li>
+						<a href='#contact' onClick={toggleMenu}>
+							Contact
+						</a>
+					</li>
+				</ul>
+			</nav>
 		</div>
 	)
 }
