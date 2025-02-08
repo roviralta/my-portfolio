c5be0d653801e6bcd6ff68cdd2c44bc8e9622ded
Sergi Roviralta <sergirovisu74@gmail.com>
2025-02-09 00:11:35 +0100

    change image to be quicker

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './About.css'
-import pict from '../../data/image.png'
+import pict from '../../data/image.webp'
 import { about } from '../../data/About'
 import ResumeButton from '../Button/ResumeButton'
 import { RiLinkedinBoxLine } from 'react-icons/ri'
