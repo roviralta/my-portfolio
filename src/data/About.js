85b07de60d7202f5e3f5244e03c3eedea0974ab8
Sergi Roviralta <sergirovisu74@gmail.com>
2025-02-14 00:39:26 +0100

    last commit till the IA change

--- src/data/About.js
+++ src/data/About.js
@@ -1,7 +1,7 @@
 const about = {
 	// all the properties are optional - can be left empty or deleted
 	name: 'Sergi Roviralta',
-	role: 'Full Stack Developer',
+	role: 'Junior Full Stack Developer',
 	description:
 		'Computer Science Engineer and aspiring Full Stack Developer with experience in hybrid apps and languages like C, Java, Python, and JavaScript. Passionate about learning, innovation, and delivering high-quality solutions. Open to collaboration on impactful projects.',
 	social: {
